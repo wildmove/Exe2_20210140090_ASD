6f729980915b086f8070f462051daccde2406be6
wildmove <muhammad.wildan,ft21@mail.umy.ac.id>
2022-11-04 15:50:47 +0700

    Add project files.

---
+++ Exe2_20210140090_ASD/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exe2_ASD
+{
+    
+           
+}
+
+// Max data num = 90 + 20 - 2 x 15 + 10 + 20
+// Max data num = 110 - 30 + 30
+// Max data num = 110
+
+// j = MA
+
+// arr = wildan
